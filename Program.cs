963fc0705e4d5d4124f5dfe28728c6be5573f14a
Jessica Pansini <68460585+jspansini@users.noreply.github.com>
2022-05-14 04:49:53 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Banco
+{
+    internal class LetsCash
+    {
+        static void Main(string[] args)
+        {
+
+            int senha = 5190;
+            int tentativa = 1;
+            bool check;
+
+            Console.WriteLine("******************************************************");
+            Console.WriteLine("**************** Banco LetsCash **********************");
+            Console.WriteLine("******************************************************\n");
+            Console.WriteLine("Você tem três chances para entrar em sua conta!\n");
+
+            do
+            {
+                Console.Write("Digite a senha para entrar: ");
+                check = (int.TryParse(Console.ReadLine(), out int inputSenha) && inputSenha > 0);
+
+                if (tentativa == 3)
+                {
+                    Console.WriteLine($"Senha bloqueada!");
+                    break;
+                }
+                else if (inputSenha != senha)
+                {
+                    Console.WriteLine($"Senha incorreta!\nVocê está na {tentativa + 1}ª chance!");
+                }
+                else
+                {
+                    Console.WriteLine("Senha correta!");
+                    break;
+                }
+                tentativa++;
+            } while (check == false || tentativa <= 3);
+        }
+    }
+}
